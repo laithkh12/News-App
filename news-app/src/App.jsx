ed5c8b873c256a70ada3012f19f93505a803501d
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-05-11 23:56:50 +0200

    Add files via upload

---
+++ news-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NewsApp from "./components/NewsApp/NewsApp";
+
+const App = () => {
+  return (
+    <div className="container">
+      <NewsApp />
+    </div>
+  );
+};
+
+export default App;
